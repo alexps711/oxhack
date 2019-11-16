b5f9c238091d08b2bdb040444e1b4ab9099bafdb
“Kenneth <kenneth.kreindler@kcl.ac.uk>
2019-11-16 13:07:31 +0000

    Added screen components

---
+++ src/screens/Client.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+class Client extends React.Component{
+    constructor(props){
+        super(props)
+    }
+
+    render(){
+        return(
+            <div>
+            <h1>Client</h1>
+            </div>
+
+        )
+    }
+}
+
+export default Client
---
+++ src/screens/User.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+class User extends React.Component{
+    constructor(props){
+        super(props)
+    }
+
+    render(){
+        return(
+            <div>
+            <h1>User</h1>
+            </div>
+
+        )
+    }
+}
+
+export default User
